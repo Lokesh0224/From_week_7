2bcb7d9d45b5ffd7e34ef6eba8cf03e748ce85f7
Lokesh0224 <lokeshk20019@gmail.com>
2025-01-21 23:45:38 +0530

    added admin route

---
+++ week8/routes/admin.js
@@ -0,0 +1,37 @@
+const {Router}= require("express")
+
+const adminRouter= Router()
+
+adminRouter.post("/signup", (req, res)=>{//signup
+    res.json({
+        message:"You're signed up"
+    })
+})
+
+adminRouter.post("/signin", (req, res)=>{//signin
+    res.json({
+        message:"You're signed in"
+    })
+})
+
+adminRouter.post("/course", (req, res)=>{//admin course creation
+    res.json({
+        message:""
+    })
+})
+
+adminRouter.get("/course", (req, res)=>{
+    res.json({
+        message:""
+    })
+})
+
+adminRouter.get("/course/bulk", (req, res)=>{//show the admin created courses
+    res.json({
+        message:""
+    })
+})
+
+module.exports={
+    adminRouter:adminRouter
+}
