4ce6729300be49beeeb4bdd051d8b84ada70e871
Lokesh0224 <lokeshk20019@gmail.com>
2025-02-04 21:15:04 +0530

    list and keys

--- week9.1/src/App.jsx
+++ week9.1/src/App.jsx
@@ -1,23 +1,28 @@
 
 function App() {
+ 
+  const todos=[{
+    key:1, 
+    title: "Go to gym", 
+    done: false
+  }, {
+    key:2,
+    title: "Eat food", 
+    done: true
+  }]
   
-  return <div style={{display: "flex"}}>
-    <Card>hi there</Card>
-    <Card><div style={{color: "green"}}>hello there</div></Card>
-  </div>
-}
-
-const Card=({children})=>{
+  const todosComponents = todos.map(todo=><Todo key={todo.key} title= {todo.title} done={todo.done}/>)
   return (
-    <div style= {{border: '1px solid #ccc', 
-      borderRadius: '5px', 
-      padding:'20px', 
-      margin: '10px', 
-      boxShadow: '2px 2px 5px rgba(0, 0, 0,0.1)'
-    }}>
-      {children}
+    <div>
+{todosComponents}
     </div>
   )
+}
+
+function Todo({title, done}){
+  return <div>
+    {title}-{done?"Done!":"Not done!"}
+  </div>
 }
 
 export default App
---
+++ week9.1/src/bits.js
@@ -0,0 +1,25 @@
+//THIS IS THE CODE FOR CHILDREN, paste this in app.jsx
+/*
+function App() {
+  
+    return <div style={{display: "flex"}}>
+      <Card>hi there</Card>
+      <Card><div style={{color: "green"}}>hello there</div></Card>
+    </div>
+  }
+  
+  const Card=({children})=>{
+    return (
+      <div style= {{border: '1px solid #ccc', 
+        borderRadius: '5px', 
+        padding:'20px', 
+        margin: '10px', 
+        boxShadow: '2px 2px 5px rgba(0, 0, 0,0.1)'
+      }}>
+        {children}
+      </div>
+    )
+  }
+  
+  export default App
+*/
