8c72481b62236351354bc0388ce8c180994d16a3
Lokesh0224 <lokeshk20019@gmail.com>
2025-02-14 16:03:08 +0530

    use of selectors and memos

--- week11_recoil_adv/src/App.jsx
+++ week11_recoil_adv/src/App.jsx
@@ -1,5 +1,5 @@
 import {RecoilRoot, useRecoilState, useRecoilValue} from 'recoil'
-import { messagingAtom, networkAtom, jobsAtom, notificationAtom } from './atom'
+import { messagingAtom, networkAtom, jobsAtom, notificationAtom, totalNotificationSelector } from './atom'
 
 function App(){
   
@@ -15,17 +15,24 @@ function MainApp(){
   const networkNotificationCount= useRecoilValue(networkAtom)
   const jobsAtomCount= useRecoilValue(jobsAtom)
   const notificationsAtomCount= useRecoilValue(notificationAtom)
-  const [messagingAtomCount, setmessagingAtomCount]= useRecoilState(messagingAtom)//here useRecoilState because its like useState , 
+  const messagingAtomCount= useRecoilValue(messagingAtom)
+  //here useRecoilState because its like useState , 
   //if you want to update the value use useRecoilState hook, useRecoilValue is used to get the value                                                                                //which will return us the value and return the fn which will update the value
   //useSetRecoilState is used to update the value but no need of rerendering 
+  const totalNotificationCount=  useRecoilValue(totalNotificationSelector)
+
+  //instead of using selector you can also use useMemo but selector got more advantages
+  // const totalNotificationCount= useMemo(()=>{
+  //   return networkNotificationCount+ jobsAtomCount+ notificationsAtomCount+ messagingAtomCount
+  // }, [networkNotificationCount, jobsAtomCount, notificationsAtomCount,  messagingAtomCount])
   return <div>
     <button>Home</button>
     <button>My Network ({networkNotificationCount >=100 ? "99+": networkNotificationCount})</button>
     <button>Jobs ({jobsAtomCount})</button>
-    <button>Messaging ({messagingAtomCount})</button>
+    <button>Messaging ({messagingAtomCount})</button> 
     <button>Notifications ({notificationsAtomCount})</button>
 
-    <button onClick={()=>{setmessagingAtomCount(c=>c+1)}}>Me </button>
+    <button>Me {totalNotificationCount}</button>
   </div>
 }
 
--- week11_recoil_adv/src/atom.js
+++ week11_recoil_adv/src/atom.js
@@ -1,4 +1,4 @@
-import { atom } from 'recoil'
+import { atom , selector} from 'recoil'
 
 export const networkAtom= atom ({
     key:"NetworkAtom", 
@@ -18,5 +18,19 @@ export const notificationAtom= atom ({
 export const messagingAtom= atom ({
     key:"MessagingAtom", 
     default: 0
+})
+
+//selectors are derived from other atoms or even other selectors 
+export const totalNotificationSelector= selector({
+    key: 'totalNotificationCountSelector', 
+    get:({get})=>{//this value is a function which gives you access to the get object
+        const networkAtomCount= get(networkAtom)
+        const jobsAtomCount= get(jobsAtom)
+        const notificationAtomCount= get(notificationAtom)
+        const messagingAtomCount= get(messagingAtom)
+
+        return networkAtomCount + jobsAtomCount + notificationAtomCount + messagingAtomCount
+
+    } 
 })
 
